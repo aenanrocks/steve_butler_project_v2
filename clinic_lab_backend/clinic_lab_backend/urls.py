10b4a847d806bea851165df51db90c1613cbf7fb
Aenan Bashir <aenanrocks@gmail.com>
2024-09-29 17:58:11 +0100

    Installed and configured Django REST Framework

--- clinic_lab_backend/clinic_lab_backend/urls.py
+++ clinic_lab_backend/clinic_lab_backend/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def test_api_view(request):
+    return Response({"message": "Hello, world!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/test/', test_api_view),
 ]
